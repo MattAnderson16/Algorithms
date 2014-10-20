57b355e0a7f9db5433c4f6b7ffed39400d1b7c21
Matthew Anderson <30393@longroad.ac.uk>
2014-10-20 16:06:58 +0100

    Binary Tree

--- Binary_Search_Tree.py
+++ Binary_Search_Tree.py
@@ -1,8 +1,13 @@
+import pdb
+
 class BinaryTree:
     def __init__(self, root):
         self.root = root
         self.left = None
         self.right = None
+
+    def __str__(self):
+        return str({"Root":self.root,"Left":self.left,"Right":self.right})
     
     def getLeftChild(self):
         return self.left
@@ -18,7 +23,8 @@ class BinaryTree:
     
     def insertLeft(self, val):
         if self.left != None:
-            if val < self.left:
+            self.left = BinaryTree(self.left)
+            if val < self.left.root:
                 self.left.insertLeft(val)
             else:
                 self.left.insertRight(val)
@@ -27,16 +33,25 @@ class BinaryTree:
     
     def insertRight(self, val):
         if self.right != None:
-            self.right.insertRight(val)
+            self.right = BinaryTree(self.right)
+            if val < self.right:
+                self.right.insertLeft(val)
+            else:
+                self.right.insertRight(val)
         else:
             self.right = val
 
 if __name__ == "__main__":
     Tree = BinaryTree(5)
-    leftChild = Tree.getLeftChild()
-    print(leftChild)
+    print(Tree)
+##    leftChild = Tree.getLeftChild()
+##    print(leftChild)
     Tree.insertLeft(4)
-    leftChild = Tree.getLeftChild()
-    print(leftChild)
+##    leftChild = Tree.getLeftChild()
+##    print(leftChild)
+    pdb.set_trace()
     Tree.insertLeft(3)
+##    leftChild = Tree.getLeftChild()
+##    print(leftChild)
+    print(Tree)
     
